44eb438124111cc08dcf84812006fd37230a887e
Bryson Palmer <brysonpalmer@gmail.com>
2021-01-01 23:43:21 -0800

    Made some style modifications to the background and all the cards. I also styled the script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// |--------------------------------Handle-Button-Click-And-Storage--------------------------------|
+
 
 // Starts the program after the button click
 function handleSearch() { 
@@ -10,7 +12,13 @@ function handleSearch() {
     // Pass the city name into the current weather request
     currentWeatherRequest( search );
 
+    
+
 }
+
+
+// |--------------------------------Open-Weather-API-Requests--------------------------------|
+
 
 // Current weather API request from open weather
 function currentWeatherRequest( search ) {
@@ -38,7 +46,6 @@ function currentWeatherRequest( search ) {
 
         // Next call `makeOneCallRequest( lat, lon )` and pass in the lat and lon
         oneCallRequest(lat, lon);
-
     });
 }
 
@@ -64,8 +71,11 @@ function oneCallRequest( lat, lon ) {
         // Render five day forecast
         renderFiveDay(res);
     });
-
 }
+
+
+// |--------------------------------Build-And-Render-Current-Weather--------------------------------|
+
 
 // Build current weather card 
 function biuldCurrentWeather (res) {
@@ -75,7 +85,6 @@ function biuldCurrentWeather (res) {
 
     // Build current weather body
     buildCurrentBody(res);    
-
 }
 
 // Build the current weather header
@@ -84,20 +93,20 @@ function buildCurrentHeader (res) {
     // Creating header div for the current weather card
     // Adding class
     let currentHeaderDiv = $("<div>");
-    currentHeaderDiv.addClass("container-fluid d-inline-flex justify-content-between flex-wrap header-box p-0");
-    
+    currentHeaderDiv.addClass("container-fluid d-flex flex-wrap justify-content-start header-box p-0");
+
     // Creating element for the city name
     // Adding class
     // Appending to the div
     let city = $("<h2>").text(res.name);
-    city.addClass("col-6 mt-3 pt-0 pr-0 pb-0 pl-3");
+    city.addClass("pl-3 mr-4");
     currentHeaderDiv.append(city); 
 
     // Convert unix time to date
     // Create an element for the date
     // Appending to the id
     let date = buildDate(res.dt);
-    $("#date-display").append(renderDate(date));
+    currentHeaderDiv.append(renderDate(date));
 
     // Render icon 
     // Appending to the div
@@ -106,10 +115,6 @@ function buildCurrentHeader (res) {
     
     // Prependng the currentHeaderDiv of current weather elements to id
     $("#current-weather").prepend(currentHeaderDiv);
-
-
-    // Set city name to local storage
-
 }
 
 // Build the current weather body
@@ -122,6 +127,7 @@ function buildCurrentBody(res) {
 
     // Convert temp
     let temp = buildTemp(res.main.temp);
+    temp.addClass("currentTemp");
     currentBodyDiv.append(temp);
     
     // Creating element for the humidity
@@ -136,8 +142,11 @@ function buildCurrentBody(res) {
     
     // Prependng the currentBodyDiv of current weather elements to id
     $("#current-weather").append(currentBodyDiv);
-
 };
+
+
+// |--------------------------------Loop-And-Create-UV-Index--------------------------------|
+
 
 // Create UVI element tag
 // Add color to the background of the index number
@@ -172,8 +181,11 @@ function renderUvi (uvIndex) {
             "<span class='col-1 uviVeryHigh'>" +
             uvIndex + "</span>" +
             "</P>");
-    
 }
+
+
+// |--------------------------------Loop-And-Build-Five-Day-Forecast--------------------------------|
+
 
 // Build the five day forecast cards
 // Create a div to hold each new element of the five day card
@@ -181,8 +193,9 @@ function renderUvi (uvIndex) {
 function renderFiveDay (res) {
 
     // Loop for each card
-    for( let i = 1; i <= 5; ++i ){
-        // Create div to hold elements
+    for( let i = 1; i <= 5; ++i ) {
+
+        // Create div to hold five day header and body elements
         let fiveDayCard = $("<div>");
         fiveDayCard.addClass("five-day d-flex flex-column text-white bg-dark col-11 m-3 pt-3 pb-3");
         
@@ -193,20 +206,24 @@ function renderFiveDay (res) {
         // Build the icon for each day
         let icon = renderIcon(res.daily[i].weather[0].icon);
         fiveDayCard.append(icon);
-
+        
         // Convert temp
         let temp = buildTemp(res.daily[i].temp.day);
+        temp.addClass("fiveDayTemp");
         fiveDayCard.append(temp);
     
-        // Creating element for the humidity
-        // Appending to the div
+        // Build the humidity
         let humidity = $("<p>").text("Humidity: " + res.daily[i].humidity + "%");
         fiveDayCard.append(humidity);
-
-        // Append the html div with newly created div of elements
+        
+        // Append the five day card to html id
         $("#five-day-card").append(fiveDayCard);
     }
 }
+
+
+// |--------------------------------Build-And-Render-Functions--------------------------------|
+
 
 // Converting unix timestamp of new date to x/xx/xxxx
 function buildDate(newDate) {
@@ -219,14 +236,14 @@ function buildDate(newDate) {
 
 // Create an element for the date
 function renderDate(convertDate) {
-    const date = $("<h3>").text(convertDate);
-    date.addClass("text-white mb-3"); 
+    const date = $("<h2>").text(convertDate);
+    date.addClass("text-white"); 
     return date;
 }
 
 // Create an image element for the icon, build the source url, and set the attribute
 function renderIcon(newIcon) {
-    const icon = $('<img class="p-0" id="weather-icon" alt="Weather icon" width="150" height="150">');
+    const icon = $('<img class="" alt="Weather icon" width="150" height="150">');
     const iconUrl = "http://openweathermap.org/img/wn/" + newIcon + "@2x.png";
     icon.attr("src", iconUrl);
     console.log(newIcon);
@@ -235,16 +252,20 @@ function renderIcon(newIcon) {
 
 // Create an element for the temp
 function buildTemp(temp) {
-    const tempF = ((temp - 273.15) * 1.80 + 32).toFixed();
-    const temperature = $("<p>").text("Temperature: " + tempF + "°F");
+    const tempF = ((temp - 273.15) * 1.80 + 32).toFixed(1);
+    const temperature = $("<p>").text("Temperature: " + tempF + " °F");
     return temperature;
 }
+
+
+// |--------------------------------Button-Click-And-Clear--------------------------------|
+
 
 // Function to empty out the articles
 function clear() {
     $("#city-name").empty();
-    $("#current-header").empty();
-    $("#date-display").empty();
+    $("#current-weather").empty();
+    $("#five-day-card").empty();
   }
 
 // .on("click") function associated with the Search Button
@@ -258,5 +279,4 @@ $("#search-btn").on("click", function(event) {
     
     // Start the application processing
     handleSearch();
-
 });
