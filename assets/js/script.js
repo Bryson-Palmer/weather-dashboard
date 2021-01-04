74eb183c7418ab24efc33a469e849ab5d659f5a2
Bryson Palmer <brysonpalmer@gmail.com>
2021-01-03 16:59:02 -0800

    Finished bulding initial query upon loading page, last searched city, and various issues related to invalid inputs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,65 @@
+// |--------------------------------Display-Last-Searched-City--------------------------------|
+
+
+init();
+
+function init() {
+    
+    // Get search from local storage
+    savedCityArray = JSON.parse(localStorage.getItem("cityObject")) || [];
+    buildDropDown(savedCityArray);
+    
+    // Get and display last city search
+    let city = savedCityArray.pop();
+
+    if ( savedCityArray.pop() == null ) {
+
+        city = "Rio De Janeiro"
+    
+        console.log("Initial city: " + city);
+    
+        // buildDropDown(savedCityArray);
+        currentWeatherRequest( city );
+        return;
+
+    } else 
+        console.log("Last city request: " + city.city);
+    
+        currentWeatherRequest( city.city );
+}
+
+
 // |--------------------------------Handle-Button-Click-And-Storage--------------------------------|
 
 
 // Starts the program after the button click
-function handleSearch() { 
+function handleSearch(city) { 
 
-    // THEN get vlue of user's input
-    let search = $("#city-name")
-    .val()
-    .trim();
-   
+    console.log("Current city request: " + city);
     // Pass the city name into the current weather request
-    currentWeatherRequest( search );
+    currentWeatherRequest( city );
 
+    // Get array of previously searched cities out from local storage
+    savedCityArray = JSON.parse(localStorage.getItem("cityObject")) || [];
+
+    // Loop over array
+    // check for dupliates using the city name
+    // Splice out duplicate city name
+    for( let i = 0; i < savedCityArray.length; ++i ) {
+        if( city == savedCityArray[i].city ) {
+            
+            savedCityArray.splice(i, 1);
+            console.log(savedCityArray);
+        }
+    }
     
-
+    let cityObject = { city: city };
+    savedCityArray.push(cityObject);
+    
+    // Setting search (city name) to local storage
+    localStorage.setItem("cityObject", JSON.stringify(savedCityArray));
+    
+    buildDropDown(savedCityArray);
 }
 
 
@@ -21,13 +67,12 @@ function handleSearch() {
 
 
 // Current weather API request from open weather
-function currentWeatherRequest( search ) {
+function currentWeatherRequest( city ) {
 
-    console.log(search);
     // Build the URL for the API request
     const APIKey = "f542952a683b86e49e9c9f66edc3747a";
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + 
-    search + "&appid=" + APIKey;
+    city + "&appid=" + APIKey;
 
     // Make the request to the URL with jQuery ajax
     $.ajax({
@@ -35,6 +80,7 @@ function currentWeatherRequest( search ) {
         method: "GET"
     }).then(function(res) {
        
+        console.log("This is the Current Weather request:");
         console.log(res);
 
         // START rendering data to the html
@@ -63,6 +109,7 @@ function oneCallRequest( lat, lon ) {
         method: "GET"
     }).then(function(res) {
         let uvi = res.current.uvi;
+        console.log("This is the One Call request:");
         console.log(res);
 
         // Render uvi to current body card
@@ -246,7 +293,6 @@ function renderIcon(newIcon) {
     const icon = $('<img class="" alt="Weather icon" width="150" height="150">');
     const iconUrl = "http://openweathermap.org/img/wn/" + newIcon + "@2x.png";
     icon.attr("src", iconUrl);
-    console.log(newIcon);
     return icon;
 }
 
@@ -258,14 +304,77 @@ function buildTemp(temp) {
 }
 
 
-// |--------------------------------Button-Click-And-Clear--------------------------------|
+// |-------------------------------Build-Drop-Down-And-Handle-Click-------------------------------|
+
+
+// Function to display previous city searches to drop down menu
+function buildDropDown(savedCityArray) {
+
+    // Create a div to hold the drop down meun
+    // Add a class
+    const dropDown = $('<div id="city-button">');
+    dropDown.addClass("list-group");
+
+    for(let i = 0; i < savedCityArray.length; ++i) {
+
+        // Create a new button for each city saved in storage
+        // Add a class
+        // Append it to the drop down div
+        let button = $(`<button type="button" data-city="dropDownCity`+[i]+`" id="dropDownedCity`+[i]+`">`).text(savedCityArray[i].city);
+        button.addClass("list-group-item list-group-item-action");
+        dropDown.append(button);
+        
+    }
+    // Append drop down to html div @ id
+    $("#city-list").append(dropDown);
+
+}
+
+
+// .on("click") function associated with the drop down Button
+$("#city-list").on("click", function(event) {
+
+    // Prevents the page from reloading on form submit.
+    event.preventDefault();
+
+    // Get vlue of user's click
+    let dropDownCity = event.target.closest("[data-city]");
+    if(dropDownCity && dropDownCity.getAttribute("data-city") !== "root") {
+        
+        // Get the text of the event target
+        let cityName = event.target.innerText;
+
+        // Check to see if a number was clicked
+        if( !isNaN(cityName)) {
+            console.log("Invalid input");
+            $("#city-name").addClass("border border-danger text-danger").val("Invalid input");
+            // location.reload();
+            return;
+            
+        } else if( $("#city-name").val() == "Invalid input" ) {
+            $("#city-name").removeClass("border-danger text-danger").val("");
+            // location.reload();
+            return;
+        } else {
+            clear();
+
+            // Start the application processing
+            handleSearch(cityName);
+        }
+    }
+        console.log("Error getting city from drop down menu.");
+});
+
+
+// |-------------------------------Search-Button-And-Input-Click-And-Clear---------------------------------|
 
 
 // Function to empty out the articles
 function clear() {
-    $("#city-name").empty();
+    $("#city-name").val("");
     $("#current-weather").empty();
     $("#five-day-card").empty();
+    $("#city-list").empty();
   }
 
 // .on("click") function associated with the Search Button
@@ -273,10 +382,34 @@ $("#search-btn").on("click", function(event) {
 
     // Prevents the page from reloading on form submit.
     event.preventDefault();
+
+    // Get vlue of user's input
+    let city = $("#city-name")
+    .val()
+    .trim()
+    .toString();
+
+    // Check to see if input is empty or a number
+    if( city == "" || !isNaN($("#city-name").val())) {
+        console.log("Invalid input");
+        $("#city-name").addClass("border border-danger text-danger").val("Invalid input");
+        return;
     
-    // Empty the search field
-    clear();
+    } else if( $("#city-name").val() == "Invalid input" ) {
+        $("#city-name").removeClass("border-danger text-danger").val("");
+        // location.reload();
+        return;
     
-    // Start the application processing
-    handleSearch();
+    } else {
+        clear();
+
+        // Start the application processing
+        handleSearch(city);
+    }
+    
+});
+
+// .on("click") function associated with the input field
+$("#city-name").on("click", function(event) {
+    $("#city-name").removeClass("border-danger text-danger").val("");
 });
